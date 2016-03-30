86d6fa06d16a7fa20f2e0c7e0dd48444d2e38492
Josh Panebianco <pana23@gmail.com>
2016-03-30 18:39:41 +1100

    end of day wed

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,21 +1,22 @@
 class MeasurementsController < ApplicationController
   def new
+    @user = User.find(params[:user_id])
     @measurement = Measurement.new
   end
 
   def create
     measurement = Measurement.new measurement_params
+    measurement.user_id = @current_user.id if @current_user
     if measurement.save
-      redirect_to root_path
+      redirect_to users_dashboard_path
     else
       render :new
     end
-
   end
 
   private
   def measurement_params
-    params.require(:measurement).permit(:height, :weight, :waist, :chest, :thighs, :stomach, :hips, :upper_arm_right, :upper_arm_left)
+    params.require(:measurement).permit(:height, :weight, :waist, :chest, :thighs, :stomach, :hips, :upper_arm_right, :upper_arm_left, :user_id)
   end
 end
 
--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -17,4 +17,18 @@
 
 class Measurement < ActiveRecord::Base
   belongs_to :user
+
+  def compare(other)
+    {
+      height: self.height.to_f - other.height.to_f,
+      weight: self.weight.to_f - other.weight.to_f,
+      waist: self.waist.to_f - other.waist.to_f,
+      chest: self.chest.to_f - other.chest.to_f,
+      thighs: self.thighs.to_f - other.thighs.to_f,
+      stomach: self.stomach.to_f - other.stomach.to_f,
+      hips: self.hips.to_f - other.hips.to_f,
+      upper_arm_right: self.upper_arm_right.to_f - other.upper_arm_right,
+      upper_arm_left: self.upper_arm_left.to_f - other.upper_arm_left
+    }
+  end
 end

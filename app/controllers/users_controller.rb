33321ba28532d43d39ef9e4049d63d41162564f7
Josh Panebianco <pana23@gmail.com>
2016-03-31 17:48:19 +1100

    final version

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,9 +33,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       render :new
+    end
+  end
+
+  def index
+    if @current_user
+      redirect_to users_dashboard_path
     end
   end
 
